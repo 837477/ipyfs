a41ab0dba6883a0bb2f8c8a68a0e454a425624e2
_seowjdals <8374770@gmail.com>
2022-07-20 15:09:52 +0900

    First Init

---
+++ ipfs_python/RPCs/__init__.py
@@ -0,0 +1,9 @@
+"""
+IPFS RPC APIs
+"""
+
+
+# IPFS RPC APIs Method
+
+
+__VERSION__ = "0.13.0"
---
+++ ipfs_python/__init__.py
@@ -0,0 +1,11 @@
+"""
+IPFS-Python
+"""
+
+
+from .ipfs import IPFS
+from .RPCs import *
+
+
+__AUTHOR__ = "837477"
+__VERSION__ = "0.0.1"
