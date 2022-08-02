b535e041c5ec2bd471fe2cc31f33e6bfd0be1a65
Hans <64589974+rhcproc@users.noreply.github.com>
2022-08-03 04:06:25 +0900

    feat: applying pep8 (79 characters)

--- examples/nft_image_uploader/main.py
+++ examples/nft_image_uploader/main.py
@@ -10,11 +10,12 @@ import json
 import os
 import re
 
+
 class NFTMetadataUploader:
     def __init__(
-            self, 
-            host: str, 
-            port: int, 
+            self,
+            host: str,
+            port: int,
             metadata_folder: str,
             images_folder: str,
             ipfs_folder: str,
@@ -24,7 +25,8 @@ class NFTMetadataUploader:
         self.ipfs_folder = ipfs_folder
         self.metadata_folder = metadata_folder
         self.images_folder = images_folder
-        self.ipfs_metadata_folder = os.path.join(ipfs_folder, metadata_folder)
+        self.ipfs_metadata_folder = os.path.join(ipfs_folder, 
+                                                        metadata_folder)
         self.ipfs_images_folder = os.path.join(ipfs_folder, images_folder)
         self.network_interval_seconds = network_interval_seconds
 
@@ -32,20 +34,24 @@ class NFTMetadataUploader:
         """
         Checks if folders exist and creates them if they don't
         """
-        try: self.files.stat(f'/{self.ipfs_folder}')
+        try:
+            self.files.stat(f'/{self.ipfs_folder}')
         except Exception as e:
             if e.args[0]['Code'] == 0:
-                print(f'IPFS({self.ipfs_folder}) Folder does not exist. Creating...')
+                print('IPFS({}) Folder does not exist. Creating...'.format(
+                                                        self.ipfs_folder))
                 self.files.mkdir(f'/{self.ipfs_folder}')
 
-        try: self.files.stat(f'/{self.ipfs_images_folder}')
-        except Exception as e: 
+        try:
+            self.files.stat(f'/{self.ipfs_images_folder}')
+        except Exception as e:
             if e.args[0]['Code'] == 0:
                 print('Image folder does not exist. Creating...')
                 self.files.mkdir(f'/{self.ipfs_images_folder}')
-        
-        try: self.files.stat(f'/{self.ipfs_metadata_folder}')
-        except Exception as e: 
+
+        try:
+            self.files.stat(f'/{self.ipfs_metadata_folder}')
+        except Exception as e:
             if e.args[0]['Code'] == 0:
                 print('Metadata folder does not exist. Creating...')
                 self.files.mkdir(f'/{self.ipfs_metadata_folder}')
@@ -54,15 +60,15 @@ class NFTMetadataUploader:
         """
         Returns all metadata files in the metadata folder
         """
-        return sorted([x for x in 
-                    os.listdir(self.metadata_folder) if ".json" in x])
+        return sorted([x for x in
+                       os.listdir(self.metadata_folder) if ".json" in x])
 
     def get_images(self) -> dict:
         """
         Returns all images in the images folder
         """
-        return { x.split('.')[0]:x 
-                    for x in os.listdir(self.images_folder) if '.' in x}
+        return {x.split('.')[0]: x
+                for x in os.listdir(self.images_folder) if '.' in x}
 
     def upload(self, create=True):
         """
@@ -71,13 +77,15 @@ class NFTMetadataUploader:
         all_metadata = self.get_all_metadata()
         images = self.get_images()
         for metadata_file in tqdm(all_metadata):
-            with open(os.path.join(self.metadata_folder, metadata_file), "r") as f_meta:
+            with open(os.path.join(
+                    self.metadata_folder, metadata_file), "r") as f_meta:
                 # Upload image to IPFS
                 count_str = re.findall(r'(\d+)', metadata_file)[0]
                 image_name = images[count_str]
                 image_path = os.path.join(self.images_folder, image_name)
                 with open(image_path, "rb") as f_image:
-                    ipfs_f_image_path = os.path.join(self.ipfs_images_folder, image_name)
+                    ipfs_f_image_path = os.path.join(
+                        self.ipfs_images_folder, image_name)
                     self.files.write(
                         path=f"/{ipfs_f_image_path}",
                         file=f_image,
@@ -88,15 +96,19 @@ class NFTMetadataUploader:
 
                 # Upload metadata to IPFS
                 metadata = json.load(f_meta)
-                metadata['name'] = metadata['name'].format(count=int(count_str))
-                metadata['image'] = metadata['image'].format(image_hash=info['result']['Hash'])
-                ipfs_f_meta_path = os.path.join(self.ipfs_metadata_folder, metadata_file)
+                metadata['name'] = metadata['name'].format(
+                    count=int(count_str))
+                metadata['image'] = metadata['image'].format(
+                    image_hash=info['result']['Hash'])
+                ipfs_f_meta_path = os.path.join(
+                    self.ipfs_metadata_folder, metadata_file)
                 self.files.write(
                     path=f"/{ipfs_f_meta_path}",
                     file=json.dumps(metadata),
                     create=True
                 )
                 sleep(self.network_interval_seconds)
+
 
 if __name__ == "__main__":
     nftmu = NFTMetadataUploader(
